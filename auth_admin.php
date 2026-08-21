738e7ace18eb8a91ddfeaa14b9194f49320409df
Baskara19 <imadebaskarasaccidananda@gmail.com>
2026-08-21 18:36:30 +0700

    Update auth_admin.php

--- auth_admin.php
+++ auth_admin.php
@@ -1,8 +1,5 @@
 <?php
-// pakai session khusus admin
-session_name("PERTASHOP_ADMIN");
 session_start();
-
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
     header("Location: ../login.php");
     exit();
